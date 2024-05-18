ba4348397ed34aea2be779c0a18f7706554c858b
vdurnea <victoria.durnea@studenti.unipd.it>
2024-05-18 11:03:57 +0200

    close #10
    test e implementazione da 1 a 20

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -18,8 +18,11 @@ public class RomanPrinterTest {
         assertEquals("__      __ _____  _____  _____ \n" + "\\ \\    / /|_   _||_   _||_   _|\n" + " \\ \\  / /   | |    | |    | |  \n" + "  \\ \\/ /    | |    | |    | |  \n" + "   \\  /    _| |_  _| |_  _| |_ \n" + "    \\/    |_____||_____||_____|\n", RomanPrinter.print(8));
         assertEquals(" _____  __   __\n" + "|_   _| \\ \\ / /\n" + "  | |    \\ V / \n" + "  | |     > <  \n" + " _| |_   / . \\ \n" + "|_____| /_/ \\_\\\n", RomanPrinter.print(9));
         assertEquals(" __   __\n" + " \\ \\ / /\n" + "  \\ V / \n" + "   > <  \n" + "  / . \\ \n" + " /_/ \\_\\\n", RomanPrinter.print(10));
+        assertEquals(" __   __ _____ __      __\n" + " \\ \\ / /|_   _|\\ \\    / /\n" + "  \\ V /   | |   \\ \\  / / \n" + "   > <    | |    \\ \\/ /  \n" + "  / . \\  _| |_    \\  /   \n" + " /_/ \\_\\|_____|    \\/    \n", RomanPrinter.print(14));
+        assertEquals(" __   __ __   __\n" + " \\ \\ / / \\ \\ / /\n" + "  \\ V /   \\ V / \n" + "   > <     > <  \n" + "  / . \\   / . \\ \n" + " /_/ \\_\\ /_/ \\_\\\n", RomanPrinter.print(20));
+
         assertEquals("Numero non valido", RomanPrinter.print(0));
-        assertEquals("Numero non valido", RomanPrinter.print(11));
+        assertEquals("Numero non valido", RomanPrinter.print(1001));
 
     }
 
